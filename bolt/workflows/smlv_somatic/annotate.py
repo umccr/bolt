8909952d9a27b5232959875ecc8b1b081140d599
qclayssen <quentin.clayssen@gmail.com>
2025-04-09 14:12:04 +1000

    work adds loggins

--- bolt/workflows/smlv_somatic/annotate.py
+++ bolt/workflows/smlv_somatic/annotate.py
@@ -43,7 +43,8 @@ def entry(ctx, **kwargs):
     output_dir = pathlib.Path(kwargs['output_dir'])
     output_dir.mkdir(mode=0o755, parents=True, exist_ok=True)
 
-    setup_logging(output_dir)
+    script_name = pathlib.Path(__file__).stem
+    setup_logging(output_dir, script_name)
 
     # Set all FILTER="." to FILTER="PASS" as required by PURPLE
     filter_pass_fp = set_filter_pass(kwargs['vcf_fp'], kwargs['tumor_name'], output_dir)
