117c707bfe2fd4ef1c08f56f20e2104324364b4c
qclayssen <quentin.clayssen@gmail.com>
2025-04-14 17:45:00 +1000

    chang command pcgr

--- bolt/common/pcgr.py
+++ bolt/common/pcgr.py
@@ -135,7 +135,7 @@ def run_somatic(input_fp, pcgr_refdata_dir, pcgr_output_dir, chunk_nbr=None, thr
     command_args = [
         f'--sample_id {sample_id}',
         f'--input_vcf {input_fp}',
-        f'--vep_dir {vep_dir}',
+        f'--vep_dir {pcgr_refdata_dir}',
         f'--refdata_dir {pcgr_refdata_dir}',
         f'--tumor_dp_tag TUMOR_DP',
         f'--tumor_af_tag TUMOR_AF',
@@ -148,8 +148,7 @@ def run_somatic(input_fp, pcgr_refdata_dir, pcgr_output_dir, chunk_nbr=None, thr
         f'--estimate_tmb',
         #f'--show_noncoding',
         f'--vcfanno_n_proc {threads}',
-        f'--vep_n_forks {threads}',
-        f'--vep_pick_order biotype,rank,appris,tsl,ccds,canonical,length,mane',
+        f'--vep_pick_order biotype,rank,appris,tsl,ccds,canonical,length,mane_plus_clinical,mane_select',
     ]
 
     # NOTE(SW): VEP pick order is applied as a successive filter:
@@ -275,6 +274,9 @@ def transfer_annotations_somatic(input_fp, tumor_name, pcgr_vcf_fp, pcgr_tsv_fp,
         constants.VcfInfo.PCGR_TCGA_PANCANCER_COUNT: 'TCGA_PANCANCER_COUNT',
         constants.VcfInfo.PCGR_CSQ: 'CSQ',
     }
+
+    pcgr_tsv_fp = pathlib.Path(pcgr_dir) / 'nosampleset.pcgr.grch38.snvs_indels.tiers.tsv'
+    pcgr_vcf_fp = pathlib.Path(pcgr_dir) / 'nosampleset.pcgr.grch38.vcf.gz'
 
     # Enforce matching defined and source INFO annotations
     check_annotation_headers(info_field_map, pcgr_vcf_fp)
