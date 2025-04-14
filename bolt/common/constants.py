b521c3759e78b5f9e3c36ef7441979e75b4fda63
qclayssen <quentin.clayssen@gmail.com>
2025-04-14 17:45:35 +1000

    change  pcgr vcf header/info

--- bolt/common/constants.py
+++ bolt/common/constants.py
@@ -236,29 +236,30 @@ VCF_HEADER_ENTRIES = {
         ),
     },
     VcfInfo.PCGR_CSQ: {
-        'Number': '.',
-        'Type': 'String',
-        'Description': (
-            'Consequence annotations from Ensembl VEP. Format: Allele|Consequence|IMPACT|SYMBOL|'
-            'Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|'
-            'CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|ALLELE_NUM|'
-            'DISTANCE|STRAND|FLAGS|PICK|VARIANT_CLASS|SYMBOL_SOURCE|HGNC_ID|CANONICAL|'
-            'MANE_SELECT|MANE_PLUS_CLINICAL|TSL|APPRIS|CCDS|ENSP|SWISSPROT|TREMBL|UNIPARC|'
-            'UNIPROT_ISOFORM|RefSeq|DOMAINS|HGVS_OFFSET|AF|AFR_AF|AMR_AF|EAS_AF|EUR_AF|SAS_AF|'
-            'gnomAD_AF|gnomAD_AFR_AF|gnomAD_AMR_AF|gnomAD_ASJ_AF|gnomAD_EAS_AF|gnomAD_FIN_AF|'
-            'gnomAD_NFE_AF|gnomAD_OTH_AF|gnomAD_SAS_AF|CLIN_SIG|SOMATIC|PHENO|CHECK_REF|'
-            'NearestExonJB'
-        ),
+    'Number': '.',
+    'Type': 'String',
+    'Description': (
+        'Consequence annotations from Ensembl VEP. Format: '
+        'Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|'
+        'HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|'
+        'ALLELE_NUM|DISTANCE|STRAND|FLAGS|PICK|VARIANT_CLASS|SYMBOL_SOURCE|HGNC_ID|CANONICAL|'
+        'MANE|MANE_SELECT|MANE_PLUS_CLINICAL|TSL|APPRIS|CCDS|ENSP|SWISSPROT|TREMBL|UNIPARC|'
+        'UNIPROT_ISOFORM|RefSeq|DOMAINS|HGVS_OFFSET|gnomADe_AF|gnomADe_AFR_AF|gnomADe_AMR_AF|'
+        'gnomADe_ASJ_AF|gnomADe_EAS_AF|gnomADe_FIN_AF|gnomADe_MID_AF|gnomADe_NFE_AF|'
+        'gnomADe_REMAINING_AF|gnomADe_SAS_AF|CLIN_SIG|SOMATIC|PHENO|CHECK_REF|MOTIF_NAME|'
+        'MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE|TRANSCRIPTION_FACTORS|NearestExonJB|'
+        'MaxEntScan_alt|MaxEntScan_diff|MaxEntScan_ref'
+    ),
     },
     VcfInfo.PCGR_MUTATION_HOTSPOT: {
         'Number': '.',
         'Type': 'String',
-        'Description': 'Known cancer mutation hotspot, as found in cancerhotspots.org_v2, Gene|Codon|Q-value',
+        'Description': 'Known cancer mutation hotspot, as found in cancerhotspots.org. Format: GeneSymbol|Entrez_ID|CodonRefAA|Alt_AA|Q-value',
     },
     VcfInfo.PCGR_CLINVAR_CLNSIG: {
         'Number': '.',
         'Type': 'String',
-        'Description': 'ClinVar clinical significance',
+        'Description': 'ClinVar - clinical significance - per phenotype submissione',
     },
     VcfInfo.PCGR_COSMIC_COUNT: {
         'Number': '1',
