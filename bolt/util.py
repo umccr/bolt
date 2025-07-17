1080ef7bc9ff0fe2862163c35d3c61da0c06d983
qclayssen <quentin.clayssen@gmail.com>
2025-07-17 11:17:44 +1000

    Fix I/O compress file and add logs

--- bolt/util.py
+++ bolt/util.py
@@ -1,3 +1,4 @@
+import gzip
 import pathlib
 import subprocess
 import sys
@@ -95,7 +96,93 @@ def get_qualified_vcf_annotation(anno_enum):
     return f'{anno_enum.namespace}/{anno_enum.value}'
 
 
-#def add_vcf_filter(record, filter_enum):
-#    existing_filters = [e for e in record.FILTERS if e != 'PASS']
-#    assert filter_enum.value not in existing_filters
-#    return ';'.join([*existing_filters, filter_enum.value])
+def merge_tsv_files(tsv_files, merged_tsv_fp):
+    """
+    Merges all TSV files into a single TSV.
+    """
+    logger.info("Merging TSV files...")
+    with gzip.open(merged_tsv_fp, 'wt') as merged_tsv:
+        for i, tsv_file in enumerate(tsv_files):
+            with gzip.open(tsv_file, 'rt') as infile:
+                for line_number, line in enumerate(infile):
+                    # Skip header except for the first file
+                    if i > 0 and line_number == 0:
+                        continue
+                    merged_tsv.write(line)
+    logger.info(f"Merged TSV written to: {merged_tsv_fp}")
+
+
+def merge_vcf_files(vcf_files, merged_vcf_fp):
+    """
+    Merges multiple VCF files into a single sorted VCF file using bcftools.
+
+    Parameters:
+    - vcf_files: List of paths to VCF files to be merged.
+    - merged_vcf_fp: Path to the output merged VCF file (without extension).
+
+    Returns:
+    - Path to the sorted merged VCF file.
+    """
+    logger.info("Merging VCF files...")
+    merged_vcf_fp = pathlib.Path(merged_vcf_fp)
+    merged_unsorted_vcf = merged_vcf_fp.with_suffix('.unsorted.vcf.gz')
+    merged_vcf = merged_vcf_fp.with_suffix('.vcf.gz')
+
+    # Prepare the bcftools merge command arguments
+    command_args = [
+        'bcftools merge',
+        '-m all',
+        '-Oz',
+        f'-o {merged_unsorted_vcf}',
+    ] + [str(vcf_file) for vcf_file in vcf_files]
+
+    # Format the command for readability
+    delimiter_padding = ' ' * 10
+    delimiter = f' \\\n{delimiter_padding}'
+    command_args_str = delimiter.join(command_args)
+
+    command = f'''
+    {command_args_str}
+    '''
+
+    # Run the bcftools merge command
+    logger.info("Running bcftools merge...")
+    execute_command(command)
+    logger.info(f"Merged VCF written to: {merged_unsorted_vcf}")
+
+    # Sort the merged VCF file
+    sort_command_args = [
+        'bcftools sort',
+        '-Oz',
+        f'-o {merged_vcf}',
+        f'{merged_unsorted_vcf}'
+    ]
+    sort_command_args_str = delimiter.join(sort_command_args)
+    sort_command = f'''
+    {sort_command_args_str}
+    '''
+
+    logger.info("Sorting merged VCF file...")
+    execute_command(sort_command)
+    logger.info(f"Sorted merged VCF written to: {merged_vcf}")
+
+    # Index the sorted merged VCF file
+    index_command_args = [
+        'bcftools index',
+        '-t',
+        f'{merged_vcf}'
+    ]
+    index_command_args_str = delimiter.join(index_command_args)
+    index_command = f'''
+    {index_command_args_str}
+    '''
+
+    logger.info("Indexing sorted merged VCF file...")
+    execute_command(index_command)
+    logger.info(f"Indexed merged VCF file: {merged_vcf}.tbi")
+
+    # Optionally, remove the unsorted merged VCF file
+    if merged_unsorted_vcf.exists():
+        merged_unsorted_vcf.unlink()
+
+    return merged_vcf
